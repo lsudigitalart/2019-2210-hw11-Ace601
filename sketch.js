21ec75863c7f4f4d65b3632f5aaa90ae99acde88
Ace601 <54597293+Ace601@users.noreply.github.com>
2019-11-22 09:12:59 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-
-var major = [174, 116, 99, 79, 69]
 var img;
+var major = [174, 116, 99, 79, 69]
+
 
 function preload(){
-img = loadImage("tiger.png")
+img = loadImage('tiger.png')
 
 }
 
@@ -12,63 +12,74 @@ img = loadImage("tiger.png")
 
 function setup(){
 createCanvas(800,800);
-
+tint(205, 127); 
  
+img.resize(800,800)
+
+
+
+
 }
 
 function draw(){
-    tint(255, 127); 
-image(img,0,0) 
-
-   background(0)
-
+    
  
 
+ background(0)
+image(img,5,-17)
+ 
+fill(250)
 for (var i = 0; i < major.length; i++) {
-    rect(i * 150 + 90, 725  - major[i], 20, major[i]);
+    rect(i * 150 + 95, 727  - major[i], 20, major[i]);
 
 
 
 }
 //majors
-fill(255)
-textSize(20)
-text('Business', 4, 730);
-fill(255)
-textSize(20);
-text('Engineering', 124, 730);
-fill(255)
+fill(170)
+//fill(255)
+textSize(18)
+text('Business', 7, 730);
+//fill(255)
+textSize(18);
+text('Engineering', 129, 730);
+//fill(255)
 textSize(22);
 text('Education', 276, 730);
-fill(255)
+//fill(255)
 textSize(22);
 text('Sciences', 432, 730);
-fill(255)
+//fill(255)
 textSize(16);
 text('Communication', 570, 730);
 
 //header
 
-fill(255)
-textSize(32)
-text('Five most popular majors for 2018 graduates', 90, 300)
+fill(50, 39, 115)
+stroke(155)
+strokeWeight(3)
+textSize(36)
+textStyle(BOLD);
+text('Five most popular majors for 2018 graduates', 13, 300)
 
 //percentile
+fill(250, 187, 45)
+noStroke()
 
-fill(255)
+//fill(255)
 textSize(20)
-text('', 4, 730);
-fill(255)
+text('24%', 86, 750);
+//fill(255)
 textSize(20);
-text('', 124, 730);
-fill(255)
+text('16%', 237, 750);
+//fill(255)
 textSize(22);
-text('', 276, 730);
-fill(255)
+text('9%', 390, 750);
+//fill(255)
 textSize(22);
-text('', 432, 730);
-fill(255)
-textSize(16);
-text('', 570, 730);
+text('7%', 542, 750);
+//fill(255)
+textSize(22);
+text('6%', 690, 750);
 
 }
